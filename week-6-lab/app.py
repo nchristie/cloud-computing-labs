e4e2b751177f2577248cfe76035aed56e161dca6
Naomi Christie <naomi.christie@fundingcircle.com>
2022-04-01 13:53:24 +0100

    Add new route

--- week-6-lab/app.py
+++ week-6-lab/app.py
@@ -7,5 +7,10 @@ def hello():
   html = "<h1>Hello {name}!</h1>".format(name="Arman")
   return html
 
+@app.route("/<name>")
+def hello(name):
+  html = f"<h1>Hello {name}!</h1>"
+  return html
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=80)
